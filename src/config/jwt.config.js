acbbddf23a166c7109d0490ca5d6d9638af76f13
Cornelius Prilly <cornelius.prilly@qoala.id>
2022-06-04 15:37:42 +0700

    Set JWT token to longer

--- src/config/jwt.config.js
+++ src/config/jwt.config.js
@@ -1,13 +1,13 @@
 export default {
   secret: "mySecretKey",
-  // jwtExpiration: 3600,         // 1 hour
-  // jwtRefreshExpiration: 86400, // 24 hours
+  jwtExpiration: 3600,         // 1 hour
+  jwtRefreshExpiration: 7200, // 2 hours
 
   /* for development */
   // jwtExpiration: 60,          // 1 minute
   // jwtRefreshExpiration: 120,  // 2 minutes
 
   /* for test */
-  jwtExpiration: 150,          // 2.5 minutes
-  jwtRefreshExpiration: 300,   // 5 minutes
+  // jwtExpiration: 150,          // 2.5 minutes
+  // jwtRefreshExpiration: 300,   // 5 minutes
 };
